2ce6fbdf0dd6a751e693e1c6e8ab4f56cff14367
AtsuH27 <hongzhonga78@gmail.com>
2024-01-13 18:12:12 +0900

    rubyの復習

---
+++ ex0201.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+puts '製品名: A5-2'
+puts '製品種別:Bluetoothスピーカー'
+
+mybook1 = '作って身につくC言語入門'
+mybook2 = 'Respberry Pi "もっと食べたい" レシピ集'
+
+puts '書籍名: 「'+mybook1+'」「'+mybook2+'」'
